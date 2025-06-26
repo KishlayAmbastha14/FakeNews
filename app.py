04236b937d63fe00deca4dec9f7301bb5bf42a0e
Kishlay Kumar <kishlaykumar087@gmail.com>
2025-06-27 02:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 import streamlit as st
 import joblib
 import os
-# import urllib.request
-import gdown
+import urllib.request
+# import gdown
 
-# model_url = "https://drive.google.com/uc?id=1TLwuRLBCQbQkKLI9-YKNzF78Y2q9JIdK"
-file_id = "1TLwuRLBCQbQkKLI9-YKNzF78Y2q9JIdK"
+model_url = "https://huggingface.co/datasets/kishlayambastha/fakenews-model/blob/main/modells1.pkl"
+# file_id = "1TLwuRLBCQbQkKLI9-YKNzF78Y2q9JIdK"
 model_path = "modells1.pkl"
-download_url = f"https://drive.google.com/uc?id={file_id}"
+# download_url = f"https://drive.google.com/uc?id={file_id}"
 
 
 st.info(f"Working directory: {os.getcwd()}")
@@ -17,23 +17,21 @@ st.info(f"Working directory: {os.getcwd()}")
 if not os.path.exists(model_path):
     try:
         st.info("Downloading model...")
-        gdown.download(download_url, model_path, quiet=False)
+        urllib.request.urlretrieve(model_url, model_path) 
         st.success("Download complete.")
     except Exception as e:
         st.error(f"Model download failed: {e}")
         st.stop()
 
 
-if not os.path.exists(model_path):
-    st.error("Model file still not found after download. Check if the link is valid or download failed.")
-    st.stop()
+# if not os.path.exists(model_path):
+#     st.error("Model file still not found after download. Check if the link is valid or download failed.")
+#     st.stop()
 try:
     model = joblib.load(model_path)
 except Exception as e:
     st.error(f"Model loading failed: {e}")
     st.stop()
-
-
 
 
 st.markdown("""
