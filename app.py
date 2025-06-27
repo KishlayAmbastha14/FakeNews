54cfcc5074984260521aef24269e5183376547f9
Kishlay Kumar <kishlaykumar087@gmail.com>
2025-06-27 09:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,25 @@ model_url = "https://huggingface.co/datasets/kishlayambastha/fakenews-model/reso
 model_path = "modells1.pkl"
 
 # st.info(f"📁 Working directory: {os.getcwd()}")
-
-if not os.path.exists(model_path):
-    try:
+@st.cache_resource(show_spinner="Loading model...")
+def load_model():
+   if not os.path.exists(model_path):
+       try:
         # st.info("📥 Downloading model...")
-        urllib.request.urlretrieve(model_url, model_path)
+           urllib.request.urlretrieve(model_url, model_path)
         # st.success("✅ Download complete.")
         # st.write("Model size:", os.path.getsize(model_path), "bytes")
-    except Exception as e:
-        st.error(f"❌ Model download failed: {e}")
-        st.stop()
+       except Exception as e:
+           st.error(f"❌ Model download failed: {e}")
+           st.stop()
 
-try:
-    model = joblib.load(model_path)
-    # st.success("✅ Model loaded successfully")
-except Exception as e:
-    st.error(f"❌ Model loading failed: {e}")
-    st.stop()
+   try:
+       return joblib.load(model_path)
+   except Exception as e:
+       st.error(f"❌ Model loading failed: {e}")
+       st.stop()
+
+model = load_model()
 
 st.markdown("""
    <h1 style='color:#53299F;'>📰 Fake News Detection</h1>
